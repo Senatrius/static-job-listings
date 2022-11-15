9bf9587e6e2a572a490670bc74d1f634ca7caf32
senatrius <g.makutenas@gmail.com>
2022-11-16 01:41:55 +0200

    add main component, adjust header background path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,13 @@ const HeaderComponent = styled.header`
   width: 100%;
   height: 9.6875rem;
   background-color: ${COLORS.primary};
-  background-image: url('bg-header-mobile.svg');
+  background-image: url('src/images/bg-header-mobile.svg');
   background-repeat: no-repeat;
   background-position: left bottom;
   background-size: cover;
 
   @media screen and (min-width: 767px) {
-    background-image: url('bg-header-desktop.svg');
+    background-image: url('src/images/bg-header-desktop.svg');
   }
 `;
 
---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from "react";
+import styled from "styled-components";
+
+const MainComponent = styled.main`
+  width: 86%;
+  max-width: 69.375rem;
+  margin: 0 auto;
+`
+
+export const Main = ({children}: PropsWithChildren) => {
+  return <MainComponent>
+    {children}
+  </MainComponent>
+}
